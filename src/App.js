c83847bfdaff739b52f83b424d2780849105e4ea
trikky <princeinme128@gmail.com>
2023-03-08 11:30:29 +0300

    Revert "update"
    This reverts commit 9b139eb7093b41f8f15caaf3cbfa4b718d9ee0e8.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Head from './components/Head';
+import AboutMe from './components/AboutMe';
+import Services from './components/Services';
+import Works from './components/Works.jsx';
+import Foot from './components/Foot';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        
+        <Head className="head"></Head>
+        <AboutMe/>
+        <Services/>
+        <Works></Works>
+        <Foot></Foot>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
